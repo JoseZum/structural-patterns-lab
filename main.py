a6ebf442a78d150e97a613b8967b6ce983917ea8
JoseMonge17 <josejulianmonge@gmail.com>
2025-10-18 14:32:25 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import sys
-import os
 
-# Agregar la carpeta facade_pattern a sys.path
-sys.path.append(os.path.join(os.path.dirname(__file__), 'Ejercicio5'))
-
-from Client import Client
-from Facade import Facade
+from Ejercicio5.Client import Client
+from Ejercicio5.Facade import Facade
 
 
 if __name__ == "__main__":
